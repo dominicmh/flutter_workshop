8ac1c05afd1f27d43fb8d08d1f3ad1a472e4732b
Oliver Gepp <oliver@gepp.name>
2019-07-22 16:06:45 +0200

    preparing empty project to start with

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Select or create a voting'),
+        ),
+        body: Center(child: Text('Nothing to see')));
+  }
+}
